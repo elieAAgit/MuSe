696e42844e89c5b75c415bdb75289e174748a15f
Elie <milhouze@live.fr>
2022-04-12 15:55:09 +0200

    Added custom class: AnimateButton, an animated version of UIButton. Created Home.storyboard and his controller HomeViewController. Version 1 of the Home.storyboard design.

---
+++ MuSe/Controller/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  MuSe
+//
+//  Created by Qattus on 12/04/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func selectCategories(_ sender: AnimateButton) {
+    }
+}
---
+++ MuSe/View/Extension/AnimateButton.swift
@@ -0,0 +1,33 @@
+//
+//  AnimateButton.swift
+//  MuSe
+//
+//  Created by Qattus on 12/04/2022.
+//
+
+import UIKit
+
+final class AnimateButton: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 12
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 1, height: 1)
+    }
+
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        animated()
+
+        return super.beginTracking(touch, with: event)
+    }
+
+    /// Add animation when button is tapped
+    private func animated() {
+
+        UIView.animate(withDuration: 0.1, animations: {
+            self.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        }, completion: { (true) in
+            self.transform = .identity
+        })
+    }
+}
