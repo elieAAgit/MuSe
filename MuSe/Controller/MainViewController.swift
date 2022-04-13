fdd86de0afb8d0bc6aaf1b364b2a2aa6841ee956
Elie <milhouze@live.fr>
2022-04-13 21:03:23 +0200

    Creation of MainCoordinator and implementation in MainViewController with variable: coordinator.

--- MuSe/Controller/MainViewController.swift
+++ MuSe/Controller/MainViewController.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class MainViewController: UIViewController, Storyboarded {
 
+    // MARK: - Property
+
+    weak var coordinator: MainCoordinator?
+
+    // MARK: - LifeCycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
+// MARK: - Method
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension MainViewController {
+    @IBAction func tapped(_ sender: UIButton) {
+        coordinator?.goToHome()
     }
-    */
-
 }
---
+++ MuSe/Coordinator/Coordinators/MainCoordinator.swift
@@ -0,0 +1,54 @@
+//
+//  MainCoordinator.swift
+//  MuSe
+//
+//  Created by Qattus on 13/04/2022.
+//
+
+import UIKit
+
+final class MainCoordinator: Coordinator {
+
+    // MARK: - Properties
+
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+
+    // MARK: - Initializer
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    // MARK: - Methods
+
+    /// Configure Main (with tabBar item)
+    func start() {
+
+        let vc = MainViewController.instantiate()
+        let nav = NavigationConfiguration(controller: vc)
+        vc.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "house.fill"), tag: 0)
+        vc.coordinator = self
+        nav.configurationNavigation(title: "Skeleton", buttonBack: "Home")
+
+        navigationController.pushViewController(vc, animated: false)
+    }
+
+    /// Go to home view
+    func goToHome() {
+
+    }
+
+    /// Remove the last controller
+    func childDidFinish(_ child: Coordinator?) {
+
+        for (index, coordinator) in
+            childCoordinators.enumerated() {
+
+            if coordinator === child {
+                childCoordinators.remove(at: index)
+                break
+            }
+        }
+    }
+}
